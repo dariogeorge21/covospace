b72c516ad8edbe8709ecb06ba9cff8e8ea03e199
Dario George <edu.dariogeorge21@gmail.com>
2025-09-30 00:03:08 +0530

    Refactor App component to separate Home function and organize routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,21 @@ import ClientMarquee from './components/ClientMarquee';
 import Blog from './components/Blog';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
+import { Routes, Route } from 'react-router-dom';
+import PrivateOffices from './pages/PrivateOffices';
+import DedicatedDesk from './pages/DedicatedDesk';
+import FlexiDesk from './pages/FlexiDesk';
+import VirtualOffices from './pages/VirtualOffices';
+import MeetingRooms from './pages/MeetingRooms';
+import About from './pages/About';
+import Location from './pages/Location';
+import OurClients from './pages/OurClients';
+import Contact from './pages/Contact';
+import CompanyRegistration from './pages/CompanyRegistration';
 
-function App() {
+function Home() {
   return (
     <div className="min-h-screen">
-      <Header />
       <Hero />
       <Services />
       <Gallery />
@@ -20,6 +30,27 @@ function App() {
       <Blog />
       <Testimonials />
       <Footer />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/private-offices" element={<PrivateOffices />} />
+        <Route path="/dedicated-desk" element={<DedicatedDesk />} />
+        <Route path="/flexi-desk" element={<FlexiDesk />} />
+        <Route path="/virtual-offices" element={<VirtualOffices />} />
+        <Route path="/meeting-rooms" element={<MeetingRooms />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="/our-clients" element={<OurClients />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/company-registration" element={<CompanyRegistration />} />
+      </Routes>
     </div>
   );
 }
